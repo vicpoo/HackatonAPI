8cfe50cab31ce6552166760d88d216da881d1848
reyhades <reyhades2005@gmail.com>
2024-07-28 00:27:33 -0600

    orderWithCoffee

--- src/order/repositories/OrderRepository.ts
+++ src/order/repositories/OrderRepository.ts
@@ -7,7 +7,7 @@ export class OrderRepository {
   // Método para obtener todas las órdenes
   public static async findAll(): Promise<Order[]> {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT order_id, date_orders, client_id FROM orders', (error, results) => {
+      connection.query('SELECT order_id, date_orders, client_id_fk FROM orders', (error, results) => {
         if (error) {
           reject(error);
         } else {
@@ -21,7 +21,7 @@ export class OrderRepository {
   // Método para obtener una orden por su ID
   public static async findById(order_id: number): Promise<Order | null> {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT order_id, date_orders, client_id FROM orders WHERE order_id = ?', [order_id], (error, results) => {
+      connection.query('SELECT order_id, date_orders, client_id_fk FROM orders WHERE order_id = ?', [order_id], (error, results) => {
         if (error) {
           reject(error);
         } else {
@@ -38,7 +38,7 @@ export class OrderRepository {
 
   // Método para crear una nueva orden
   public static async createOrder(order: Order): Promise<Order> {
-    const query = 'INSERT INTO orders (date_orders, client_id) VALUES (?, ?)';
+    const query = 'INSERT INTO orders (date_orders, client_id_fk) VALUES (?, ?)';
     return new Promise((resolve, reject) => {
       connection.execute(query, [order.date_orders, order.client_id_fk], (error, result: ResultSetHeader) => {
         if (error) {
@@ -96,7 +96,7 @@ export class OrderRepository {
 
   // Método para actualizar una orden por su ID
   public static async updateOrder(orderId: number, order: Order): Promise<void> {
-    const query = 'UPDATE orders SET date_orders = ?, client_id = ? WHERE order_id = ?';
+    const query = 'UPDATE orders SET date_orders = ?, client_id_fk = ? WHERE order_id = ?';
     return new Promise((resolve, reject) => {
       connection.execute(query, [order.date_orders, order.client_id_fk, orderId], (error) => {
         if (error) {
