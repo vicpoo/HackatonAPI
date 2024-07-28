1979d22756cc2db9f7306fe2ce03e864954c5416
reyhades <reyhades2005@gmail.com>
2024-07-28 11:47:51 -0600

    orderWithCoffee

--- src/order/routes/orderRoutes.ts
+++ src/order/routes/orderRoutes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import { getOrder, getOrderById, createOrder, updateOrder, deleteOrder, createOrderWithCoffees } from '../controllers/orderControllers';
-import { authMiddleware } from '../../shared/middlewares/auth';
 
 const orderRoutes: Router = Router();
 
 orderRoutes.get('/', getOrder);
-orderRoutes.get('/:order_id', authMiddleware, getOrderById);
+orderRoutes.get('/:order_id', getOrderById);
 orderRoutes.post('/', createOrder);
 orderRoutes.post('/with-coffees', createOrderWithCoffees);
 orderRoutes.put('/:order_id', updateOrder);
